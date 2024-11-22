191880c14a0ae18fd3337a93d269739f31eeb469
kouki0423 <juri890nako3240@gmail.com>
2024-11-22 15:36:08 +0900

    Update Station1.java

--- src/station1/Station1.java
+++ src/station1/Station1.java
@@ -9,11 +9,22 @@ public class Station1 {
 
     public static ArrayList<String> q1() {
         // 問題1: ここから
+        ArrayList<String> list=new ArrayList<>();
+        list.add("Tech");
+        list.add("Train");
+        return list;
         // 問題1: ここまで
     }
 
     public static int q2(ArrayList<String> list, ArrayList<String> searchWords) {
         // 問題2: ここから
+        int count=0;
+        for(String s:searchWords){
+            if(list.count(s)){
+                count++;
+            }
+        }
+        return count;
         // 問題2: ここまで
     }
 }
